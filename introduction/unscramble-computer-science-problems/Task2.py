5abacc51f65922a83a54252c9333f976a56216de
Victoria Oke <42435608+Kweentoria@users.noreply.github.com>
2026-02-21 16:50:36 +0000

    Updated Task2.py

--- introduction/unscramble-computer-science-problems/Task2.py
+++ introduction/unscramble-computer-science-problems/Task2.py
@@ -21,5 +21,32 @@ Print a message:
 September 2016.".
 """
 #receiving number (1)
-for row in calls[:2]:
-    print(row[1])
+
+def longestDuration(calls):
+    unique_num_with_duration = {}
+
+    for call in calls:
+        caller_number = call[0]
+        receiver_number = call[1]
+        duration = int(call[3])
+        #caller
+        if caller_number in unique_num_with_duration:
+            unique_num_with_duration[caller_number] += duration
+        else:
+            unique_num_with_duration[caller_number] = duration
+        #receiver
+        if receiver_number in unique_num_with_duration:
+            unique_num_with_duration[receiver_number] += duration
+        else:
+            unique_num_with_duration[receiver_number] = duration
+
+    longest_time = 0
+    phone = ''
+
+    for key, value in unique_num_with_duration.items():
+        if value > longest_time:
+            longest_time = value
+            phone = key
+    print("{} spent the longest time, {} seconds, on the phone during September 2016.".format(phone, longest_time))
+
+longestDuration(calls)
