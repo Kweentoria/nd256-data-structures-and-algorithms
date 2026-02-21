30d3560a4495138c2ac65d14991d9003cfc33fea
Victoria Oke <42435608+Kweentoria@users.noreply.github.com>
2026-02-21 14:53:47 +0000

    Updated Task0.py

---
+++ introduction/unscramble-computer-science-problems/Task1.py
@@ -0,0 +1,34 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files.
+"""
+import csv
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+
+"""
+TASK 1:
+How many different telephone numbers are there in the records? 
+Print a message:
+"There are <count> different telephone numbers in the records."
+"""
+
+def unique_records(texts, calls):
+    checklist = set()
+    for text in texts:
+        checklist.add(text[0])
+        checklist.add(text[1])
+        
+    for call in calls:
+        checklist.add(call[0])
+        checklist.add(call[1])
+           
+    print("There are {} different telephone numbers in the records.".format(len(checklist)))
+
+unique_records(texts, calls)
---
+++ introduction/unscramble-computer-science-problems/Task2.py
@@ -0,0 +1,25 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files
+"""
+import csv
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+    # print("Last record of calls, {} calls {} at time {}, lasting {} seconds".format(calls[0][0], calls[0][1], calls[0][2], calls[0][3]) )
+
+"""
+TASK 2: Which telephone number spent the longest time on the phone
+during the period? Don't forget that time spent answering a call is
+also time spent on the phone.
+Print a message:
+"<telephone number> spent the longest time, <total time> seconds, on the phone during 
+September 2016.".
+"""
+#receiving number (1)
+for row in calls[:2]:
+    print(row[1])
